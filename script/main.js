b5f8791f94dc90dcc4747e4ebef25e1f35441dee
Abhay Vincent <abhaykvincent@gmail.com>
2020-03-10 02:32:57 -0400

    logic complete

--- script/main.js
+++ script/main.js
@@ -9,7 +9,19 @@ let game = {
         current: null,
         score: 0
     },
-    play:["Rock","Paper","Scissors"]
+    currentResult:null,
+    play:["Rock","Paper","Scissors"],
+    gamePlay:[{
+        set: "Rock",
+        kill: "Scissors"
+    },{
+        set: "Paper",
+        kill: "Rock"
+    },{
+        set: "Scissors",
+        kill: "Paper"
+    }],
+    log:["Click Ready to start"]
 }
 
 //functions
@@ -19,9 +31,11 @@ function resetTurn(){
     game.playerB.ready=false;
     game.playerA.current=null;
     game.playerB.current=null;
+    game.playerB.currentResult=null;
     $(".ready").show();
 }
 function playerReady(){
+    $(".gameLog").html(game.log[game.log.length-1]);
     $(".playerAready").click(function (e) { 
         e.preventDefault();
         $(this).addClass("playerReady");
@@ -54,7 +68,7 @@ function getHandImg(hand,player){
     else if(hand == "Paper"){
         url= `paper_${side}.png`;
     }
-    else{
+    else if(hand == "Scissors"){
         url= `scissors_${side}.png`;
     }
     return url;
@@ -62,13 +76,62 @@ function getHandImg(hand,player){
 function startGame(){
     game.playerA.current = rockPaperScissors();
     game.playerB.current = rockPaperScissors();
+
     let handA = game.playerA.current;
     let handB = game.playerB.current;
 
+    console.log(handA);
+    console.log(handB);
+
     $(".handA").attr("src",`img/${getHandImg(handA,"A")}`);
-    resetTurn();
     $(".handB").attr("src",`img/${getHandImg(handB,"B")}`);
 
+    if(handA == handB){
+        game.currentResult= "Draw";
+        game.log.push("Draw");
+    }
+    else if(handA == "Rock"){
+        if(handB == "Scissors"){
+            game.currentResult= "A";
+            game.playerA.score++;
+            game.log.push("Rock hits Scissors");
+        }
+        else{
+            game.currentResult= "B";
+            game.playerB.score++;
+            game.log.push("Paper wraps Rock");
+        }
+    }
+    else if(handA == "Paper"){
+        if(handB == "Rock"){
+            game.currentResult= "A";
+            game.playerA.score++;
+            game.log.push("Paper wraps Rock");
+        }
+        else{
+            game.currentResult= "B";
+            game.playerB.score++;
+            game.log.push("Scissors cuts Paper");
+        }
+    }
+    else if(handA == "Scissors"){
+        if(handB == "Paper"){
+            game.currentResult= "A",
+            game.playerA.score++;
+            game.log.push("Scissors cuts Paper");
+        }
+        else{
+            game.currentResult= "B";
+            game.playerB.score++;
+            game.log.push("Rock hits Scissors");
+        }
+    }
+    console.log("Result : "+game.currentResult);
+    console.log(game.playerA.current+" A : "+game.playerA.score);
+    console.log(game.playerB.current+" B : "+game.playerB.score);
+    $(".scoreA").html(game.playerA.score);
+    $(".scoreB").html(game.playerB.score);
+    $(".ready").show();
 }
 function checkReady(){
     $(".ready").click(function (e) { 
@@ -76,7 +139,11 @@ function checkReady(){
         if(game.playerA.ready == true && game.playerB.ready == true )
         {
             startGame();
-            $(".ready").hide();
+            $(".ready").removeClass("playerReady");
+            game.playerA.ready=false;
+            game.playerB.ready=false;
+            $(".gameLog").html(game.log[game.log.length-1]);
+            console.log(game.log[game.log.length-1]);
         }
     });
 }
